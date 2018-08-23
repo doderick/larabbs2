3ff8ce0373bfac62e032bf6b8f7b663177788439
doderick <doderick@outlook.com>
2018-08-23 15:11:57 +0000

    Prevent XSS injection

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -27,6 +27,9 @@ class TopicObserver
      */
     public function saving(Topic $topic)
     {
+        // XSS 过滤
+        $topic->body = clean($topic->body, 'user_topic_body');
+
         $topic->excerpt = make_excerpt($topic->body);
     }
 }
