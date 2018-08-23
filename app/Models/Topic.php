c4aad80036fd4feea367a51c2e5d786bbe0f542d
doderick <doderick@outlook.com>
2018-08-23 04:47:07 +0000

    Topic index page

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,4 +5,24 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    /**
+     * 处理话题和用户之间的关联，一个话题属于一个用户
+     *
+     * @return void
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * 处理话题和分类之间的关联，一个话题属于一个分类
+     *
+     * @return void
+     */
+    public function Category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
