be5f4a523731f1fabfd6210fe399c00717455226
doderick <doderick@outlook.com>
2018-08-25 01:38:47 +0000

    User can create replies

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -6,12 +6,13 @@ use App\Models\Reply;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ReplyRequest;
+use Auth;
 
 class RepliesController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth', ['except' => ['index', 'show']]);
+        $this->middleware('auth');
     }
 
 	public function index()
@@ -30,10 +31,25 @@ class RepliesController extends Controller
 		return view('replies.create_and_edit', compact('reply'));
 	}
 
-	public function store(ReplyRequest $request)
+    /**
+     * 用户进行回复的方法
+     *
+     * @param ReplyRequest $request 回复请求
+     * @param Reply $reply          回复的一个实例
+     * @return void
+     */
+    public function store(ReplyRequest $request, Reply $reply)
 	{
-		$reply = Reply::create($request->all());
-		return redirect()->route('replies.show', $reply->id)->with('message', 'Created successfully.');
+        if (empty($content)) {
+            return redirect()->back()->with('danger', '回复内容无法识别！');
+        }
+
+        $reply->content  = $request->content;
+        $reply->topic_id = $request->topic_id;
+        $reply->user_id  = AUth::id();
+        $reply->save();
+
+		return redirect()->to($reply->topic->link())->with('success', '回复成功！');
 	}
 
 	public function edit(Reply $reply)
@@ -55,6 +71,6 @@ class RepliesController extends Controller
 		$this->authorize('destroy', $reply);
 		$reply->delete();
 
-		return redirect()->route('replies.index')->with('message', 'Deleted successfully.');
+		return redirect()->route('replies.index')->with('success', '删除回复成功！');
 	}
 }
--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -9,9 +9,28 @@ use App\Models\Reply;
 
 class ReplyObserver
 {
+    /**
+     * 回复创建时执行的动作
+     *
+     * @param Reply $reply 创建的回复实例
+     * @return void
+     */
     public function creating(Reply $reply)
     {
-        //
+        // 预防 XSS 攻击
+        $reply->content = clean($reply->content, 'user->topic->body');
+    }
+
+    /**
+     * 回复被创建后执行的动作
+     *
+     * @param Reply $reply 回复的一个实例
+     * @return void
+     */
+    public function created(Reply $reply)
+    {
+        // 回复数 +1
+        $reply->topic->increment('reply_count', 1);
     }
 
     public function updating(Reply $reply)
