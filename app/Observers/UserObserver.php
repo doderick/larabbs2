8f0d39fccaba73098fdd223e8536ae2add2015c6
doderick <doderick@outlook.com>
2018-08-28 06:49:26 +0000

    User default avatar after signup

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -18,4 +18,18 @@ class UserObserver
     {
         //
     }
+
+    /**
+     * 用户模型在保存时的动作
+     *
+     * @param User $user 用户模型的一个实例
+     * @return void
+     */
+    public function saving(User $user)
+    {
+        // 给用户添加默认头像
+        if (empty($user->avatar)) {
+            $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/30/1/TrJS40Ey5k.png';
+        }
+    }
 }
