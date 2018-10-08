dfc20349c023dcd22c7bbe0ee50d3249b72ea808
doderick <doderick@outlook.com>
2018-10-08 10:52:47 +0800

    Ready for Heroku

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -57,3 +57,33 @@ function model_plural_name($model)
     // 获取字串的复数形式
     return str_plural($snake_case_name);
 }
+
+/**
+ * 选择 Heroku 的数据库为 PostgreSQL
+ *
+ * @return void
+ */
+function get_db_config()
+{
+    if (getenv('IS_IN_HEROKU')) {
+        $url = parse_url(getenv('DATABASE_URL'));
+
+        return $db_config = [
+            'connection' => 'pgsql',
+            'host'       => $url['host'],
+            'port'       => $url['port'],
+            'database'   => substr($url['path'], 1),
+            'username'   => $url['user'],
+            'password'   => $url['pass']
+        ];
+    } else {
+        return $db_config = [
+            'connection' => env('DB_CONNECTION', 'mysql'),
+            'host'       => env('DB_HOST', 'localhost'),
+            'port'       => env('DB_PORT', '3306'),
+            'database'   => env('DB_DATABASE', 'forge'),
+            'username'   => env('DB_USERNAME', 'forge'),
+            'password'   => env('DB_PASSWORD', '')
+        ];
+    }
+}
