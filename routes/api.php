242d762a74b90de1de3a91fdf7cef9834af5281a
doderick <doderick@outlook.com>
2018-10-16 12:12:14 +0800

    api/user

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,8 @@ use Illuminate\Http\Request;
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
-    'namespace' => 'App\Http\Controllers\Api'
+    'namespace'  => 'App\Http\Controllers\Api',
+    'middleware' => 'serializer:array'
 ], function($api) {
 
     // 调用频率限制
@@ -45,5 +46,22 @@ $api->version('v1', [
             ->name('api.authorizations.update');
         $api->delete('authorizations/current', 'AuthorizationsController@destroy')
             ->name('api.authorizations.destroy');
+    });
+
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit'      => config('api.rate_limits.sign.limit'),
+        'exptres'    => config('api.rate_limits.sign.expires'),
+    ], function($api) {
+        // 游客可以访问的接口
+
+        // 需要 token 验证的接口
+        $api->group([
+            'middleware' => 'api.auth',
+        ], function($api) {
+            // 当前登录用户信息
+            $api->get('user', 'UsersController@me')
+                ->name('api.user.show');
+        });
     });
 });
